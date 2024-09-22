bd702c235bf7f2f3141321f6828045e8cadede4f
Mahesh Kamble <maheshkamble2805@gmail.com>
2024-09-22 23:24:03 +0530

    Update 2datatypes.js

---
+++ Basic/3conversion.js
@@ -0,0 +1,31 @@
+let score = "mahesh";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "11" => 11
+// "11abc" => NaN
+// true =>1; false =>0;  
+
+
+
+
+let IsLoggedIn = "";
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "abcd" =>true
+
+
+
+
+let someNumber = 24;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
