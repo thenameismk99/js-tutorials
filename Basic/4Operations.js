1a029b2b17df7a1f9b1b743a638694d2e29e00a6
Mahesh Kamble <maheshkamble2805@gmail.com>
2024-09-24 21:31:42 +0530

    Update 4Operations.js

---
+++ Basic/4Operations.js
@@ -0,0 +1,29 @@
+// Operaions
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Mahesh";
+let str3 = str1 + str2;
+
+// console.log(str3);
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 =num3 = 3 +3;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
